2de083ada896c177f9af8bc0cf4c056f21433cc8
Aschay <ayadi.chayma@gmail.com>
2021-02-26 12:03:58 +0100

    adding jenkins job dsl for build, package and release

---
+++ jenkins/jobs/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('simple-nodeJS-react-npm-docker-app') {
+    scm {
+        git('https://github.com/Aschay/simple-node-js-react-npm-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('aschay/docker-nodejs-react')
+            tag('testing')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
